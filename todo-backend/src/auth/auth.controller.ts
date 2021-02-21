b8ecbaafbdf019b278127fa986db4789d93b9f98
cereme(Sangyun Pae) <ceremebsy@gmail.com>
2021-02-21 16:03:54 +0900

    Use simple for auth guards

--- todo-backend/src/auth/auth.controller.ts
+++ todo-backend/src/auth/auth.controller.ts
@@ -7,28 +7,27 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
-import { GoogleAuthGuard } from './google-guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(AuthGuard('local'))
   @Post('/login/local')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(GoogleAuthGuard)
+  @UseGuards(AuthGuard('google'))
   @Get('/google')
   async withGoogle() {}
 
   @Get('/google/callback')
-  @UseGuards(GoogleAuthGuard)
+  @UseGuards(AuthGuard('google'))
   @Redirect(process.env.AUTH_REDIRECT_URL, 302)
   async withGoogleCallback(
     @Request() req,
